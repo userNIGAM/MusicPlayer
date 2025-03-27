d25bc5575dd658cbd72313281684dfc29d75fd46
Nigam Subedi <168841788+userNIGAM@users.noreply.github.com>
2025-03-27 21:52:24 +0545

    Add files via upload
    Just add songs in the assets folder and give name like song1..song20... and add image inside assets folder inside image folder like image1...image20...

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { React } from "react";
+import Musicplayer from "../src/Components/Musicplayer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Musicplayer />
+    </>
+  );
+}
+
+export default App;
---
+++ music.js
@@ -0,0 +1,165 @@
+import song1 from "./song1.mp3";
+import song2 from "./song2.mp3";
+import song3 from "./song3.mp3";
+import song4 from "./song4.mp3";
+import song5 from "./song5.mp3";
+import song6 from "./song6.mp3";
+import song7 from "./song7.mp3";
+import song8 from "./song8.mp3";
+import song9 from "./song9.mp3";
+import song10 from "./song10.mp3";
+import song11 from "./song11.mp3";
+import song12 from "./song12.mp3";
+import song13 from "./song13.mp3";
+import song14 from "./song14.mp3";
+import song15 from "./song15.mp3";
+import song16 from "./song16.mp3";
+import song17 from "./song17.mp3";
+import song18 from "./song18.mp3";
+import song19 from "./song19.mp3";
+import song20 from "./song20.mp3";
+
+import image1 from "./Images/image1.jpg";
+import image2 from "./Images/image2.jpg";
+import image3 from "./Images/image3.jpg";
+import image4 from "./Images/image4.jpg";
+import image5 from "./Images/image5.jpg";
+import image6 from "./Images/image6.jpg";
+import image7 from "./Images/image7.jpg";
+import image8 from "./Images/image8.jpg";
+import image9 from "./Images/image9.jpg";
+import image10 from "./Images/image10.jpg";
+import image11 from "./Images/image11.jpg";
+import image12 from "./Images/image12.jpg";
+import image13 from "./Images/image13.jpg";
+import image14 from "./Images/image14.jpg";
+import image15 from "./Images/image15.jpg";
+import image16 from "./Images/image16.jpg";
+import image17 from "./Images/image17.jpg";
+import image18 from "./Images/image18.jpg";
+import image19 from "./Images/image19.jpg";
+import image20 from "./Images/image20.jpg";
+
+const musicList = [
+  {
+    id: 1,
+    name: "Tujhe Kitna Cahane Lage",
+    song: song1,
+    image: image1,
+  },
+  {
+    id: 2,
+    name: "Aao Kavi Haweli Pe",
+    song: song2,
+    image: image2,
+  },
+  {
+    id: 3,
+    name: "Abhi Kuch Dino se",
+    song: song3,
+    image: image3,
+  },
+  {
+    id: 4,
+    name: "Amit Trivedi - Lehrein",
+    song: song4,
+    image: image4,
+  },
+  {
+    id: 5,
+    name: "Khamoshiyan - Arijit Sing",
+    song: song5,
+    image: image5,
+  },
+  {
+    id: 6,
+    name: "Shayad",
+    song: song6,
+    image: image6,
+  },
+  {
+    id: 7,
+    name: "Hosanna",
+    song: song7,
+    image: image7,
+  },
+  {
+    id: 8,
+    name: "Darasal",
+    song: song8,
+    image: image8,
+  },
+  {
+    id: 9,
+    name: "Peheli Dafa",
+    song: song9,
+    image: image9,
+  },
+  {
+    id: 10,
+    name: "Baatein Ye kabhi Na(Male)",
+    song: song10,
+    image: image10,
+  },
+  {
+    id: 11,
+    name: "Banjaara",
+    song: song11,
+    image: image11,
+  },
+  {
+    id: 12,
+    name: "Bin Tere",
+    song: song12,
+    image: image12,
+  },
+  {
+    id: 13,
+    name: "Channa Ve",
+    song: song13,
+    image: image13,
+  },
+  {
+    id: 14,
+    name: "Dil meri Na sune",
+    song: song14,
+    image: image14,
+  },
+  {
+    id: 15,
+    name: "Gerua",
+    song: song15,
+    image: image15,
+  },
+  {
+    id: 16,
+    name: "Hamari Adhuri Kahani",
+    song: song16,
+    image: image16,
+  },
+  {
+    id: 17,
+    name: "Hangover",
+    song: song17,
+    image: image17,
+  },
+  {
+    id: 18,
+    name: "Hasi ban gaye",
+    song: song18,
+    image: image18,
+  },
+  {
+    id: 19,
+    name: "Humdard",
+    song: song19,
+    image: image19,
+  },
+  {
+    id: 20,
+    name: "Hamko Pyaar Hua",
+    song: song20,
+    image: image20,
+  },
+];
+export default musicList;
